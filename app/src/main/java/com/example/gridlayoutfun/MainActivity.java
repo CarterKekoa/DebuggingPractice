28d488e96244ff611305d163c9f132f99f77574e
CarterMooring <carterkekoa@gmail.com>
2019-10-15 09:48:47 -0700

    First Try

--- app/src/main/java/com/example/gridlayoutfun/MainActivity.java
+++ app/src/main/java/com/example/gridlayoutfun/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.gridlayoutfun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view){
-        Button button = (Button) view;
-        String text = button.getText().toString(); //use the text in the button to know which one
+        ImageButton button = (ImageButton) view;
+        String text = button.getTag().toString(); //use the text in the button to know which one
+        //tag, used for storing extra info about the view
         Toast.makeText(this, "Hello from button " + text, Toast.LENGTH_SHORT).show();
     }
 }
